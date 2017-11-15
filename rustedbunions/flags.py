8f6bf38228e9488f8b1f027225b66e98cd9434d7
Sean LaPlante <laplante.sean@gmail.com>
2017-11-14 19:59:18 -0500

    Fix for linting

--- rustedbunions/flags.py
+++ rustedbunions/flags.py
@@ -50,4 +50,6 @@ FLAGS = {
     "stego1":("flag{TEST44}", 10),
     "stego2":("flag{TEST45}", 15),
     "stego3":("flag{TEST46}", 25),
+    "github_issue":("flag{TEST47}", 10),
+    "github_commit":("flag{TEST48}", 10)
 }
