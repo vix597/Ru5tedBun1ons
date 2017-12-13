b1631897e6a0699083a5b93d0d13f4d498bdd73c
Sean LaPlante <laplantes@ainfosec.com>
2017-12-13 16:25:07 -0500

    Added useful comment

--- rustedbunions/core/templatetags/js.py
+++ rustedbunions/core/templatetags/js.py
@@ -1,3 +1,12 @@
+'''
+With this you can do the following in a HTML template file:
+
+{% load js %}
+var x = "{{ some_context_var | js}}"; // This is a JSON representation of the variable
+
+Not perfect or guarenteed to work in all scenarios, however it's a good way to send python
+dicts as context variables and load them into javascript variables client-side.
+'''
 import json
 
 from django.utils.safestring import mark_safe
