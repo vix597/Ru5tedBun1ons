91561ebbd6b03796961775013f5e5846db66ba07
Sean LaPlante <laplantes@ainfosec.com>
2017-12-19 11:56:27 -0500

    Fix issue in leaderboard model. Added 2 new fields

--- rustedbunions/leaderboard/models.py
+++ rustedbunions/leaderboard/models.py
@@ -16,12 +16,12 @@ class LeaderboardEntry(models.Model):
     playtime = models.CharField(verbose_name="Total Playtime", max_length=25)
     percent_complete = models.IntegerField(default=0, verbose_name="Percent Complete")
     secret_key = models.CharField(default="", max_length=128, verbose_name="Secret Key")
-    leader_oid = models.CharField(default=ObjectId(), max_length=32, verbose_name="Leader OID")
+    leader_oid = models.CharField(default=ObjectId, max_length=32, verbose_name="Leader OID")
     session_creation_time = models.DateTimeField(default=timezone.now, verbose_name="Session Creation Time")
     purchased_challenges = models.CharField(default="", max_length=4000, verbose_name="Purchased Challenges")
     remote_ip = models.CharField(default="", max_length=64, verbose_name="Remote IP")
-    last_modified = models.DateTimeField(auto_now=True)
-    leader_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True, verbose_name="Last Modified")
+    leader_created = models.DateTimeField(auto_now_add=True, verbose_name="Leader Created Time")
 
     def __str__(self):
         return "{} - {}".format(self.name, self.lifetime_hacker_bucks)
