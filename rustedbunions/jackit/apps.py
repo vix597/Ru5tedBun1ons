89cf15127efc0f3394db38f776f8d9816e8ea1fc
unknown <laplantes@ainfosec.com>
2019-04-05 16:03:29 -0400

    Remove WIP - jackit

--- rustedbunions/jackit/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JackitConfig(AppConfig):
-    name = 'jackit'
--- rustedbunions/jackit/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = "jackit"
-urlpatterns = [
-    url(r'(?P<session_id>\w+)/$', views.index, name="index"),
-]
